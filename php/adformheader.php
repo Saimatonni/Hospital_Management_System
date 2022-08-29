d83c658167f4a368110907360606a55b52ef82c4
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2022-08-29 21:32:56 +0600

    Update adformheader.php

--- php/adformheader.php
+++ php/adformheader.php
@@ -11,7 +11,7 @@ $tim = date("H:i:s");
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-<title>HMS by ITSC</title>
+<title>Hospital Services</title>
 <link rel="icon" href="favicon.ico" type="image/x-icon">
 <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
